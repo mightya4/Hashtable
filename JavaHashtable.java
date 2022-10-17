200919cb0b193c907559dbef542877aea2ac47c5
Akeem <akeem.mighty@gmail.com>
2022-10-16 21:24:45 -0400

    Created Book Entries in Hashtable + Output Key/Value Pair

--- JavaHashtable.java
+++ JavaHashtable.java
@@ -3,5 +3,16 @@ import java.util.Hashtable;
 public class JavaHashtable {
     public static void main(String[] args) {
         Hashtable<Integer, String> ht = new Hashtable<Integer, String>();
+        ht.put(201, "To Kill A Mockingbird");
+        ht.put(202, "The Hunger Games");
+        ht.put(203, "Water for Elephants");
+        ht.put(204, "Lord of the Flies");
+        ht.put(205, "The Da Vinci Code");
+
+        ht.forEach((key, value) -> {
+            System.out.println(key + " "
+                                + value);
+        });
+
     }
 }
